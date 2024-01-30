5a74c5fcc18bcdb7dd51378d5e3f01f4a2027b31
Luke Mayhew <luke@webuildlegends.com>
2024-01-30 16:39:17 -0700

    Add express demo from https://github.com/oven-sh/bun/issues/3775#issuecomment-1892843091

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const PORT = parseInt(process.env.PORT || "8080");
+const app = express();
+
+function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+app.get("/rolldice", (req, res) => {
+  res.send(getRandomNumber(1, 6).toString());
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening for requests on http://localhost:${PORT}`);
+});
